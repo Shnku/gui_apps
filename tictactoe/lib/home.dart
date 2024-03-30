a8a74e35accca7a55c5c86da7f24aa78a205c8ad
ShAnku <101804633+Shnku@users.noreply.github.com>
2024-03-30 23:55:20 +0530

    added winnr alert..+ modify design... but player name not specified

--- tictactoe/lib/home.dart
+++ tictactoe/lib/home.dart
@@ -8,45 +8,89 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  var a = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
-  String? s;
+  var boxes = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  var tapped = [false, false, false, false, false, false, false, false, false];
+  String status = "";
   bool flag = false;
-  Widget element(int count, {VoidCallback? tapped}) {
+  int inserted = 0;
+
+  // ignore: non_constant_identifier_names
+  void val_input_logic(int indx) {
+    if (!tapped[indx]) {
+      flag = flag ? false : true;
+      inserted++;
+      boxes[indx] = flag ? "O" : "X";
+      tapped[indx] = true;
+      setState(() {});
+    }
+    status = chek_won(boxes);
+    if (status == "WIN" || inserted == 9) {
+      // Future.delayed(const Duration(seconds: 2), () {});
+      showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            elevation: 20,
+            actionsAlignment: MainAxisAlignment.center,
+            backgroundColor: Colors.blueGrey,
+            shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(20)),
+            ),
+            title: Text(inserted == 9
+                ? status == "WIN"
+                    ? "YOU WON"
+                    : "DRAW"
+                : "YOU WIN"),
+            alignment: Alignment.center,
+            content: const Text(
+              "Do you want to play again?",
+              style: TextStyle(color: Colors.black),
+            ),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                  reset(boxes, tapped); //this state is needed..
+                  setState(() {
+                    status = ""; //cant pass by referance
+                    inserted = 0;
+                  });
+                },
+                child: const Text(
+                  "OK",
+                  style: TextStyle(color: Colors.black),
+                ),
+              )
+            ],
+          );
+        },
+      );
+    }
+  }
+
+  Widget element(int which, {VoidCallback? tapped}) {
     //typedef VoidCallback = void Function()
     return InkWell(
-      onTap: tapped ??
-          () {
-            flag = flag ? false : true;
-            setState(() {
-              if (flag) {
-                a[count] = "A";
-              } else {
-                a[count] = "B";
-              }
-              s = (chek_won(a).toString());
-              if (s == "won") {
-                Future.delayed(const Duration(seconds: 3), () {
-                  reset(a);
-                  setState(() {
-                    s = null;
-                  });
-                });
-              }
-            });
-          },
+      onTap: tapped ?? () => val_input_logic(which),
+      borderRadius: const BorderRadius.all(Radius.circular(20)),
       splashFactory: InkSparkle.splashFactory,
+      highlightColor: const Color.fromARGB(255, 71, 94, 222),
       splashColor: Colors.blue,
       hoverColor: Colors.black,
       focusColor: Colors.black,
       child: Container(
         alignment: Alignment.center,
-        height: 100,
+        height: 170,
         width: 100,
-        color: Colors.red,
-        child: Text(a[count],
-            style: const TextStyle(
-              fontSize: 30,
-            )),
+        margin: const EdgeInsets.all(1),
+        decoration: BoxDecoration(
+            color: Colors.blueGrey.withAlpha(100),
+            borderRadius: const BorderRadius.all(Radius.circular(20))),
+        child: Text(
+          boxes[which] == "O" || boxes[which] == "X" ? boxes[which] : "",
+          style: const TextStyle(fontSize: 30),
+        ),
       ),
     );
   }
@@ -63,39 +107,41 @@ class _HomePageState extends State<HomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const SizedBox(height: 20),
-            Container(
-              height: 360,
-              color: Colors.amberAccent,
-              child: GridView(
-                padding: const EdgeInsets.all(10),
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 3,
-                  crossAxisSpacing: 10,
-                  mainAxisSpacing: 10,
-                  childAspectRatio: 1.5,
+            FittedBox(
+              child: Container(
+                height: 390,
+                width: 360,
+                margin: const EdgeInsets.all(8),
+                // color: Colors.amberAccent,
+                child: GridView(
+                  padding: const EdgeInsets.all(10),
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3,
+                    crossAxisSpacing: 10,
+                    mainAxisSpacing: 10,
+                    childAspectRatio: 1,
+                    // mainAxisExtent: 80,
+                  ),
+                  children: [
+                    element(0),
+                    element(1),
+                    element(2),
+                    element(3),
+                    element(4),
+                    element(5),
+                    element(6),
+                    element(7),
+                    element(8)
+                  ],
                 ),
-                children: [
-                  element(0),
-                  element(1),
-                  element(2),
-                  element(3),
-                  element(4),
-                  element(5),
-                  element(6),
-                  element(7),
-                  element(8),
-                ],
               ),
             ),
-            Text('Home Page ::=>   ${flag.toString()}'),
-            const SizedBox(height: 10),
-            Text(s ?? "",
-                style: TextStyle(fontSize: 30, color: Colors.red.shade900)),
+            Text('Home Page ::=>   ${flag.toString()} $inserted'),
             const SizedBox(height: 20),
             Column(
               mainAxisSize: MainAxisSize.max,
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: display(a),
+              children: display(boxes, tapped),
             )
           ],
         ),
@@ -111,7 +157,7 @@ String chek_won(var arr) {
         arr[i + 1] == arr[i + 2] &&
         arr[i + 2] == arr[i] &&
         arr[i] != 0) {
-      return "won";
+      return "WIN";
     }
   }
   for (var i = 0; i < 3; i++) {
@@ -119,30 +165,31 @@ String chek_won(var arr) {
         arr[i + 3] == arr[i + 6] &&
         arr[i + 6] == arr[i] &&
         arr[i] != 0) {
-      return "won";
+      return "WIN";
     }
   }
   if (arr[0] == arr[4] && arr[4] == arr[8] && arr[4] != 0) {
-    return "won";
+    return "WIN";
   }
   if (arr[2] == arr[4] && arr[4] == arr[6] && arr[4] != 0) {
-    return "won";
+    return "WIN";
   }
-  return "null";
+  return "";
 }
 
-List<Widget> display(arr) {
+List<Widget> display(arr, tap) {
   return <Widget>[
-    const Text("____________________"),
-    Text("${arr[0]}    ${arr[1]}    ${arr[2]}"),
-    Text("${arr[3]}    ${arr[4]}    ${arr[5]}"),
-    Text("${arr[6]}    ${arr[7]}    ${arr[8]}"),
-    const Text("____________________")
+    const Text("_________________________"),
+    Text("${arr[0]}) ${tap[0]}   ${arr[1]}) ${tap[1]}   ${arr[2]}) ${tap[2]}"),
+    Text("${arr[3]}) ${tap[3]}   ${arr[4]}) ${tap[4]}   ${arr[5]}) ${tap[5]}"),
+    Text("${arr[6]}) ${tap[6]}   ${arr[7]}) ${tap[7]}   ${arr[8]}) ${tap[8]}"),
+    const Text("_________________________")
   ];
 }
 
-void reset($arr) {
+void reset($arr, $tapped) {
   for (var i = 0; i < 9; i++) {
     $arr[i] = (i + 1).toString();
+    $tapped[i] = false;
   }
 }
