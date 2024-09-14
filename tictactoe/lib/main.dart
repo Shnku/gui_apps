c2f7ced5c8fb8c0699d52c1adecedb493f769518
ShAnku <101804633+Shnku@users.noreply.github.com>
2024-09-14 14:41:10 +0530

    tictactoe app fix page renaimingad refactoring

--- tictactoe/lib/main.dart
+++ tictactoe/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:myapp/home.dart';
+import 'package:tictactoe/home.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 void main() {
